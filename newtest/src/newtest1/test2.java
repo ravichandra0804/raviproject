475a9b4a6a3d89a83bd9e583da2696b7537d2859
Viswa <Viswa@DESKTOP-KBEBKCQ>
2021-04-26 19:54:55 +0530

    commit 1

---
+++ newtest/src/newtest1/test2.java
@@ -0,0 +1,13 @@
+package newtest1;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		System.out.println("newcode");
+		int a=1;
+		if(a==1) {
+			System.out.println(a);
+		}
+	}
+
+}
